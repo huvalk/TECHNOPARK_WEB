0e01af1d19b6696937c0833cf6351f0ec6acb350
huvalk <akiraq@yandex.ru>
2019-10-29 19:06:43 +0300

    Templates are fine. No paginator. Creating models

--- question/models.py
+++ question/models.py
@@ -66,7 +66,7 @@ class Answer(models.Model):
         verbose_name_plural = 'Answers'
 
     def __str__(self):
-        return self.correct
+        return "%r" % self.correct
 
 
 class TagPivot(models.Model):
